19343372bd7e9f1cf9600c1fd4fcd1c2b4c4bfb4
Le Huy <volehuy012@gmail.com>
2025-12-15 14:26:12 +0700

    Fix auto push state

--- auto_push.py
+++ auto_push.py
@@ -1,11 +1,11 @@
 import subprocess
 
 def git_push():
-    try:
-        subprocess.run(["python3", "convert.py"], check=True)
-        subprocess.run(["git", "add", "."], check=True)
-        subprocess.run(["git", "commit", "-m", "Auto update pothole map"], check=False)
-        subprocess.run(["git", "push", "origin", "main"], check=True)
-        print("Da push Data")
-    except Exception as e:
-        print("Push loi:", e)
+    subprocess.run(["git", "add", "potholes_data.json", "potholes.geojson", "images"], check=True)
+    subprocess.run(
+        ["git", "commit", "-m", "Auto update pothole data"],
+        check=False
+    )
+    subprocess.run(["git", "push", "origin", "main"], check=True)
+
+
