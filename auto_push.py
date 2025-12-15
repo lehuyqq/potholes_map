f8bb9605b048def3ff69d4d14047c31720135eaa
Le Huy <volehuy012@gmail.com>
2025-12-15 15:07:22 +0700

    Auto update pothole map

--- auto_push.py
+++ auto_push.py
@@ -1,15 +1,11 @@
 import subprocess
 
 def git_push():
-    try:
-        subprocess.run(["git", "pull", "--rebase", "origin", "main"], check=True)
-        subprocess.run(["python3", "convert.py"], check=True)
-        subprocess.run(["git", "add", "."], check=True)
-        subprocess.run(
-            ["git", "commit", "-m", "Auto update pothole data"],
-            check=False
-        )
-        subprocess.run(["git", "push", "origin", "main"], check=True)
-        print("Da push thanh cong")
-    except Exception as e:
-        print("Da xay ra loi:", e)
+    subprocess.run(["python3", "convert.py"], check=True)
+    subprocess.run(["git", "add", "-A"], check=True)
+    subprocess.run(
+        ["git", "commit", "-m", "Auto update pothole map"],
+        check=False
+    )
+    subprocess.run(["git", "push", "origin", "main"], check=True)
+
